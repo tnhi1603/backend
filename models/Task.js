fe3eae4416e18d42d82240ebbfaee794fd344d55
dT230702 <tantb888@gmail.com>
2024-12-21 13:16:23 +0700

    Update task controller
    use aggregate to query reference model

--- models/Task.js
+++ models/Task.js
@@ -1,15 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: String,
-    priority: { type: String, enum: ['Low', 'Medium', 'High'], default: 'Medium' },
-    status: { type: String, enum: ['In Progress', 'Completed'], default: 'In Progress' },
-    dueDate: Date,
-    project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
-    startDate: Date,
-    updateTime: Date,
-    idUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: { type: String, required: true },
+  description: String,
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Medium",
+  },
+  status: {
+    type: String,
+    enum: ["In Progress", "Completed"],
+    default: "In Progress",
+  },
+  dueDate: Date,
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project",
+    required: true,
+  },
+  startDate: Date,
+  updateTime: Date,
+  idUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
-module.exports = mongoose.model('Task', TaskSchema);
+module.exports = mongoose.model("Task", TaskSchema);
