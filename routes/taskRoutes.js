9f492c72bdf4173b86435a520a01d728c85a8ff4
dT230702 <tantb888@gmail.com>
2024-12-26 19:47:36 +0700

    Filter, Sort, Find  API
    Filter by status, priority
    Sort by status, priority
    Find by title, name, desc

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,9 +6,19 @@ const {
   createTask,
   updateTask,
   deleteTask,
+  getTaskListByPrioritySort,
+  getTaskListByStatusSort,
+  getTaskListByPriorityFilter,
+  getTaskListByStatusFilter,
+  findByUserInput,
 } = require("../controllers/taskController");
 
 router.get("/", getTaskList);
+router.get("/sort/priority", getTaskListByPrioritySort);
+router.get("/sort/status", getTaskListByStatusSort);
+router.get("/filter/priority", getTaskListByPriorityFilter);
+router.get("/filter/status", getTaskListByStatusFilter);
+router.get("/find", findByUserInput);
 router.get("/:id", getTask);
 router.post("/", createTask);
 router.put("/:id", updateTask);
